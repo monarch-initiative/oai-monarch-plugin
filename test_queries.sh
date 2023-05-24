6bbb562f14ef2da54399ee0cad3e111ca00323f5
Shawn O'Neil <oneilsh@gmail.com>
2023-05-24 09:39:36 -0700

    Updated to use the new Monarch dev api. Note that frequency and onset information is now returned just as an id, and causal/non-causal gene associations appear to not be working (getting causal only?)

--- test_queries.sh
+++ test_queries.sh
@@ -4,9 +4,9 @@ echo "Testing Search"
 curl -X GET "http://localhost:3434/search?term=COVID-19&category=disease&rows=2"
 
 echo -e "\n\nTesting diseease -> gene associations"
-curl -X GET "http://localhost:3434/disease-genes?disease_id=DOID:678&max_results=10&association_type=both"
+curl -X GET "http://localhost:3434/disease-genes?disease_id=MONDO:0019391&max_results=10&association_type=both"
 
 echo -e "\n\nTesting diseease -> phenotype associations"
-curl -X GET "http://localhost:3434/disease-phenotypes?disease_id=MONDO:0017309&phenotypes&rows=2"
+curl -X GET "http://localhost:3434/disease-phenotypes?disease_id=MONDO:0019391&phenotypes&rows=2"
 
 
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -25,88 +25,77 @@ def start_server():
 
 BASE_URL = "http://localhost:3434"
 
-#dev = "uvicorn oai_monarch_plugin.main:app --host 0.0.0.0 --port 3333 --reload"
-#dev = "python:uvicorn.main.run(app='oai_monarch_plugin.main:app', host='0.0.0.0', port=3333, reload=True)"
-
-
-# @pytest.mark.usefixtures("start_server")
-# def test_search_endpoint():
-#     response = requests.get(f"{BASE_URL}/search/entity/COVID-19?category=disease&rows=2")
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert "results" in data
-#     assert len(data["results"]) == 2
-
-
-# @pytest.mark.usefixtures("start_server")
-# def test_disease_gene_associations_endpoint():
-#     response = requests.get(f"{BASE_URL}/bioentity/disease/DOID:678/genes?max_results=10&association_type=both")
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert "genes" in data
-#     assert len(data["genes"]) == 10
-#     for gene in data["genes"]:
-#         assert "gene_id" in gene
-#         assert "gene_label" in gene
-#         assert "relation_label" in gene
-
-
-# @pytest.mark.usefixtures("start_server")
-# def test_disease_phenotype_associations_endpoint():
-#     response = requests.get(f"{BASE_URL}/bioentity/disease/MONDO:0017309/phenotypes?rows=2")
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert "associations" in data
-#     assert "numFound" in data
-#     assert len(data["associations"]) == 2
-#     for association in data["associations"]:
-#         assert "id" in association
-#         assert "frequency" in association
-#         assert "onset" in association
-#         assert "phenotype" in association
-#         assert "id" in association["phenotype"]
-#         assert "label" in association["phenotype"]
-#         assert association["onset"]["id"] is None or isinstance(association["onset"]["id"], str)
-#         assert association["onset"]["label"] is None or isinstance(association["onset"]["label"], str)
-
 
 @pytest.mark.usefixtures("start_server")
 def test_search_endpoint():
-    response = requests.get(f"{BASE_URL}/search?term=COVID-19&category=disease&rows=2")
+    response = requests.get(f"{BASE_URL}/search?term=COVID-19&category=biolink:Disease&rows=2")
     assert response.status_code == 200
     data = response.json()
     assert "results" in data
     assert len(data["results"]) == 2
 
+
 @pytest.mark.usefixtures("start_server")
-def test_disease_gene_associations_endpoint():
-    response = requests.get(f"{BASE_URL}/disease-genes?disease_id=DOID:678&max_results=10&association_type=both")
-    assert response.status_code == 200
+def test_disease_genes_endpoint():
+    response = requests.get(f"{BASE_URL}/disease-genes?disease_id=MONDO:0019391&max_results=2")
+
+    # Basic assertions
+    assert response.status_code == 200, f"Expected status code 200 but received {response.status_code} with message: {response.text}"
+
+    # Ensure response is valid JSON
     data = response.json()
-    assert "genes" in data
-    assert len(data["genes"]) == 10
+    print(data)
+
+    # Check for presence of 'genes' in the response
+    assert "genes" in data, "Response body did not contain expected 'genes' field"
+
+    # Check that the number of returned genes matches the max_results parameter
+    assert len(data["genes"]) == 2, f"Expected 2 genes but received {len(data['genes'])}"
+
+    # Check structure of each gene
     for gene in data["genes"]:
         assert "gene_id" in gene
         assert "gene_label" in gene
         assert "relation_label" in gene
 
+    # Ensure gene_id, gene_label, and relation_label are strings
+    for gene in data['genes']:
+        assert isinstance(gene['gene_id'], str), f"Expected 'gene_id' to be a string but found {type(gene['gene_id'])}"
+        assert isinstance(gene['gene_label'], str), f"Expected 'gene_label' to be a string but found {type(gene['gene_label'])}"
+        assert isinstance(gene['relation_label'], str), f"Expected 'relation_label' to be a string but found {type(gene['relation_label'])}"
+
+
+
 @pytest.mark.usefixtures("start_server")
 def test_disease_phenotype_associations_endpoint():
-    response = requests.get(f"{BASE_URL}/disease-phenotypes?disease_id=MONDO:0017309&rows=2")
-    assert response.status_code == 200
+    response = requests.get(f"{BASE_URL}/disease-phenotypes?disease_id=MONDO:0017309&limit=2")
+
+    # Basic assertions
+    assert response.status_code == 200, f"Expected status code 200 but received {response.status_code} with message: {response.text}"
+
+    # Ensure response is valid JSON
     data = response.json()
-    assert "associations" in data
-    assert "numFound" in data
-    assert len(data["associations"]) == 2
+
+    # Check for presence of 'associations' and 'numFound' in the response
+    assert "associations" in data, "Response body did not contain expected 'associations' field"
+    assert "numFound" in data, "Response body did not contain expected 'numFound' field"
+
+    # Check that the number of returned associations matches the limit parameter
+    assert len(data["associations"]) == 2, f"Expected 2 associations but received {len(data['associations'])}"
+
+    # Check structure of each association
     for association in data["associations"]:
         assert "id" in association
-        assert "frequency" in association
-        assert "onset" in association
+        assert "frequency_qualifier" in association
+        assert "onset_qualifier" in association
         assert "phenotype" in association
         assert "id" in association["phenotype"]
         assert "label" in association["phenotype"]
-        assert association["onset"]["id"] is None or isinstance(association["onset"]["id"], str)
-        assert association["onset"]["label"] is None or isinstance(association["onset"]["label"], str)
+
+    # Check that numFound is an integer
+    assert isinstance(data['numFound'], int), f"Expected 'numFound' to be an integer but found {type(data['numFound'])}"
+
+
 
 
 if __name__ == "__main__":
